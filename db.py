c7542e0dead3b58934d8ff0971fc4184037aace2
Shambhavi Jha <163523155+27Shambhavi@users.noreply.github.com>
2026-02-12 00:40:52 +0530

    Delete db.py

--- db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Asuccessfullperson#78",
-        database="policynav"
-    )
